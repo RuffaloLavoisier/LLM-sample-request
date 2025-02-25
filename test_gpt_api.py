998f01da4d464a63d0410a56d72ee1b9f683eaf5
Ruffalo Lavoisier <RuffaloLavoisier@gmail.com>
2025-02-25 17:39:59 +0900

    Add provider param and check condition
    for api key and provider

--- test_gpt_api.py
+++ test_gpt_api.py
@@ -7,19 +7,37 @@ def get_response(url,
                  model,
                  sys_prompt,
                  usr_prompt,
-                 api_key):
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}"
-    }
-    
-    request_body = {
-        "model": model,
-        "messages": [
-            {"role": "system", "content": sys_prompt},  
-            {"role": "user", "content": usr_prompt}
-        ]
-    }
+                 api_key,
+                 provider):
+    headers = {}
+    if api_key is not None:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_key}"
+        }
+    else:
+        headers = {
+            "Content-Type": "application/json",
+        }
+
+    request_body = {}
+    if provider is not None:
+        request_body = {
+            "model": model,
+            "provider": provider,
+            "messages": [
+                {"role": "system", "content": sys_prompt},
+                {"role": "user", "content": usr_prompt}
+            ]
+        }
+    else:
+        request_body = {
+            "model": model,
+            "messages": [
+                {"role": "system", "content": sys_prompt},
+                {"role": "user", "content": usr_prompt}
+            ]
+        }
 
     json_body = json.dumps(request_body).encode("utf-8")
 
@@ -48,13 +66,15 @@ if __name__ == "__main__":
     parser.add_argument('--sys-prompt', type=str, default='default system context.', help='Your system property.')
     parser.add_argument('--usr-prompt', type=str, default='hello, world', help='Your command.')   
     parser.add_argument('--api-key', type=str, help='Your API Key for llm service.')
+    parser.add_argument('--provider', type=str, help='Your API provider for llm service.')
     args = parser.parse_args()
 
-    url = args.url
-    model = args.model
-    sys_prompt = args.sys_prompt
-    usr_prompt = args.usr_prompt
-    api_key = args.api_key
+    url         = args.url
+    model       = args.model
+    sys_prompt  = args.sys_prompt
+    usr_prompt  = args.usr_prompt
+    api_key     = args.api_key
+    provider    = args.provider
 
-    response = get_response(url, model, sys_prompt, usr_prompt, api_key)
+    response = get_response(url, model, sys_prompt, usr_prompt, api_key, provider)
     print(response)
